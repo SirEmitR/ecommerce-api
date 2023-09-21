34e637481e4ede18480227dc2d95cc729919f7ba
EMILIANO REYES <emit3reyes@gmail.com>
2023-09-20 18:29:28 -0600

    First

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,14 @@
+import {stripe} from '@/utils/stripe'
+import { NextRequest } from 'next/server';
+
+export async function GET(req: NextRequest){
+    const params = req.nextUrl.searchParams;
+    const startAfter = params.get('startAfter');
+    const limit = params.get('limit');
+    const products = await stripe.products.list({
+        active: true,
+        limit: limit ? Number(limit) : 10,
+        starting_after: startAfter ? String(startAfter) : undefined,
+    });
+    return Response.json(products);
+}
